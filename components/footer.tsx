ebe9fcbb8c3bdd5e76b823152d71aaf974d68b77
META4722 <shiqier472@gmail.com>
2025-09-24 15:15:07 +0800

    2025

--- components/footer.tsx
+++ components/footer.tsx
@@ -38,7 +38,7 @@ export function Footer() {
         <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row md:py-0">
           <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
             <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-              &copy; 2024 Rendaily. All rights reserved.
+              &copy; 2025 Rendaily. All rights reserved.
             </p>
           </div>
         </div>
@@ -77,7 +77,7 @@ export function Footer() {
         </div>
         <div className="mt-16 flex flex-col items-center justify-between gap-4 border-t pt-8 md:flex-row">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            &copy; 2024 Rendaily. All rights reserved.
+            &copy; 2025 Rendaily. All rights reserved.
           </p>
         </div>
       </div>
